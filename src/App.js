72ec5b4c07003c6a23c325c010fed72574548ba9
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2020-05-28 23:56:34 +0900

    add front page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
+import dct from './document.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-main-contents">
+
+        {/*  <img src={logo} className="App-logo" alt="logo" />   */}
+          <h1>document-app</h1>
+          <img src={dct} className="App-dct" alt="dct" />
+
+          <p>
+            document-appはNePPの有志達によって作られた文書管理アプリ？です。<br />
+            フロントエンドはReact、バックエンドをgoで作られています。
+          </p>
+{/*          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+*/}
+        </div>
       </header>
     </div>
   );
